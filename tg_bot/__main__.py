c17e89ce408b0a90cf5f886ade6cebc6adb1f008
Rascal72 <123348045+Rascal72@users.noreply.github.com>
2023-04-09 09:27:13 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -152,7 +152,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
                     [[InlineKeyboardButton(text="🎉 Add me to your group", url="t.me/{}?startgroup=true".format(bot.username))],
                      [InlineKeyboardButton(text="👥 Support Group", url="https://t.me/aboutme_sanu"), InlineKeyboardButton(text="🔔 Update Channel", url="https://t.me/sanubots")],
-                     [InlineKeyboardButton(text="🎬 Movie Channel", url="sanusmoviesch"), InlineKeyboardButton(text="🛠 Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
+                     [InlineKeyboardButton(text="🎬 Movie Channel", url="t.me/sanusmoviesch"), InlineKeyboardButton(text="🛠 Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
 
     else:
         update.effective_message.reply_text("ചത്തിട്ടില്ലാ...")
